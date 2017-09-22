19da94adbe51cec2a97264f71144abc4bb7e13fb
Sabin <spradh@users.noreply.github.com>
2017-09-21 21:50:33 -0400

    Update reducer.py

--- Intro to Hadoop and MapReduce/Lesson 6/Quiz- Highest Sale/reducer.py
+++ Intro to Hadoop and MapReduce/Lesson 6/Quiz- Highest Sale/reducer.py
@@ -1,1 +1,35 @@
 #highest sale reducer.py
+#!/usr/bin/python
+
+import sys
+
+maxSale = 0
+oldKey = None
+
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    thisKey, thisSale = data_mapped[0],float(data_mapped[1])
+	
+
+    if oldKey and oldKey != thisKey:
+        print oldKey, "\t", maxSale
+        oldKey = thisKey;
+        maxSale = 0
+
+    oldKey = thisKey
+    if(thisSale>maxSale):
+		maxSale=thisSale
+
+if oldKey != None:
+    print oldKey, "\t", maxSale

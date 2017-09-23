e438f90c277e02af6b7d525981f945ec14d6922b
Sabin <spradh@users.noreply.github.com>
2017-09-23 12:48:55 -0400

    Create reducer.py

---
+++ Intro to Hadoop and MapReduce/Lesson 6/Quiz- Total Sales/reducer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys
+
+totalSales = 0
+numberSales=0
+oldKey = None
+
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    thisKey, thisSale = data_mapped[0],float(data_mapped[1])
+    numberSales+=1
+    totalSales+=thisSale
+
+ 	   
+
+#if oldKey != None:
+print "Total Sales Amount\t", totalSales
+print "Total Number of Sales\t",numberSales

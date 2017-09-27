cb886ede9f9cf40621c2f7fddb19c163756714b1
Sabin <spradh@users.noreply.github.com>
2017-09-27 00:22:49 -0400

    Update mapper.py

--- Intro to Hadoop and MapReduce/Lesson 6/Quiz- Hits to Page/mapper.py
+++ Intro to Hadoop and MapReduce/Lesson 6/Quiz- Hits to Page/mapper.py
@@ -1,1 +1,22 @@
 #mapper.py
+
+
+#!/usr/bin/python
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (store name) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
+
+import re
+import sys
+
+regex='([(\d\.)]+) - - \[(.*?)\] "(/*?)" (\d+) - "(.*?)" "(.*?)"'
+
+for line in sys.stdin:
+  data = line.strip().split("\t")
+  if len(data) == 6:
+  ip, identity, username, time, method, status, size= re.match(regex,data).groups()
+    if(methog)
+    print "{0}\t{1}".format(item, cost)
